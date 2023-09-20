447b72005d36a9e4a0e38dd60e804428b2ce0e80
CoreyBabs <coreymbabcock@gmail.com>
2023-09-19 21:29:35 -0600

    Add initial todo setup

---
+++ todo/src/index.js
@@ -0,0 +1,4 @@
+import { Todo } from './todo';
+
+let test = new Todo("Test", "A test item", "09/18/2023", "1", false);
+console.log(test);
---
+++ todo/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo List",
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};

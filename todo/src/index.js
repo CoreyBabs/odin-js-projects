ff88a96f6ed984d635dcc1204188395091d84162
CoreyBabs <coreymbabcock@gmail.com>
2023-09-23 23:41:42 -0600

    Update config and add basic UI

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,5 +1,7 @@
+import './styles.css';
 import { Todo } from './todo';
 import { Project } from './project';
+
 
 let test = new Todo("Test", "A test item", "09/18/2023", "1", false);
 console.log(test);
@@ -9,4 +11,5 @@ let test2 = new Todo("Test2", "A second test item", "09/19/2023", "2", false);
 let proj = new Project("Test Project", [test, test2]);
 console.log(proj);
 
+let container = document.querySelector("body");
 
--- todo/webpack.config.js
+++ todo/webpack.config.js
@@ -1,20 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Todo List",
-    }),
-  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
@@ -28,7 +21,4 @@ module.exports = {
     static: './dist',
   },
   devtool: 'inline-source-map',
-  optimization: {
-    runtimeChunk: 'single',
-  },
 };

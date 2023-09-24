4413c12555606b45ec851f1df51262ea0360603a
CoreyBabs <coreymbabcock@gmail.com>
2023-09-24 10:00:03 -0600

    Add todo table display

--- todo/src/index.js
+++ todo/src/index.js
@@ -21,7 +21,39 @@ function updateProjectSelection(firstLoad) {
     select.appendChild(opt);
     if (!firstLoad && i === projects.length - 1) {
       select.value = proj.nameValue();
+      selectedProj = proj;
     }
+  }
+}
+
+function updateTodoTable() {
+  let table = document.querySelector("#todo-table");
+  for (let todo of selectedProj.todos) {
+    let row = table.insertRow(table.rows.length);
+
+    let title = row.insertCell(0);
+    title.innerHTML = todo.title;
+
+    let description = row.insertCell(1);
+    description.innerHTML = todo.description;
+
+    let due = row.insertCell(2);
+    due.innerHTML = todo.dueDate;
+
+    let prio = row.insertCell(3);
+    prio.innerHTML = todo.priority;
+
+    let complete = row.insertCell(4);
+    let comp_chk = document.createElement("input");
+    comp_chk.setAttribute("type", "checkbox");
+    comp_chk.checked = todo.checked;
+    console.log(todo);
+    complete.appendChild(comp_chk);
+
+    let del = row.insertCell(5);
+    let del_btn = document.createElement("button");
+    del_btn.innerText = "Delete";
+    del.appendChild(del_btn);
   }
 }
 
@@ -40,12 +72,12 @@ function addNewProject() {
 let test = new Todo("Test", "A test item", "09/18/2023", "1", false);
 console.log(test);
 
-let test2 = new Todo("Test2", "A second test item", "09/19/2023", "2", false);
+let test2 = new Todo("Test2", "A second test item", "09/19/2023", "2", true);
 
 let proj = new Project("Test Project", [test, test2]);
 console.log(proj);
 
-let today = new Project("Today", []);
+let today = new Project("Today", [test, test2]);
 
 let projects = [today, proj];
 
@@ -53,4 +85,5 @@ updateProjectList(true);
 
 let addProj = document.querySelector("#proj-add");
 addProj.addEventListener("click", () => addNewProject(false));
-
+let selectedProj = today;
+updateTodoTable();
